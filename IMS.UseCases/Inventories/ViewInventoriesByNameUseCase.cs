db6cf2b37d8717b8d65ab79f0a2cde4c4a0d22fd
Tomislav Robic <tomislav.robic@supersport.hr>
2023-03-21 13:33:56 +0100

    Inventory Management System

---
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,21 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.Inventories.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await inventoryRepository.GetInventoriesByNameAsync(name);
+        }
+    }
+}
---
+++ IMS.UseCases/PluginInterfaces/IInventoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.UseCases.PluginInterfaces
+{
+    public interface IInventoryRepository
+    {
+        Task AddInventoryAsync(Inventory inventory);
+        Task<bool> ExistsAsync(Inventory inventory);
+        Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name);
+        Task<Inventory> GetInventoryByIdAsync(int inventoryId);
+        Task UpdateInventoryAsync(Inventory inventory);
+    }
+}
